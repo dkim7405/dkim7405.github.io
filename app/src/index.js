9217036ae788fe770c4f54127878bfb80f916de7
Dongyeon Kim <dongkim7405@gmail.com>
2024-09-10 12:53:55 -0400

    Initial Commit
    Tailwindcss and React Setup

---
+++ app/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './index.css';
+
+
+const Portfolio = () => {
+  return (
+    <main>
+      <h1>Portfolio</h1>
+    </main>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Portfolio />);
